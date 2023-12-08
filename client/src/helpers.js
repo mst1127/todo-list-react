cf459616454a3eb327fd02ae6e091b62d9928ee9
Michelle Tang <shmtang@hotmail.com>
2023-12-08 12:12:53 -0800

    fix: modified api url

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://todo-list-api-hnnk.onrender.com"
-export const API_URL = "http://localhost:3001"
+export const API_URL = "https://todo-list-api-hnnk.onrender.com"
+// export const API_URL = "http://localhost:3001"
